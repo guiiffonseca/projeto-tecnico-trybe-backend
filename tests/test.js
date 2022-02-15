90ccb5e870d49d546fc48ca5343ae0ad3a3b227d
Guilherme Fonseca <guii.ffonseca22@gmail.com>
2022-02-15 18:06:52 -0300

    [Add] Adiciona camadas e estabelece conexao com o banco

---
+++ tests/test.js
@@ -0,0 +1,12 @@
+describe('Initial test', () => {
+    it('test should be true', () => {
+        const result = true;
+        expect(result).toBe(true)
+    })
+})
+
+describe('Testando a rota Adicionar nova tarefa', () => {
+    it('É possível adicionar uma nova tarefa com sucesso', () => {
+
+    })
+})
